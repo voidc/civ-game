a512ea202d5bfc0a98ea211864618aab41c44b5e
Dominik Stolz <dominik.stolz21@gmail.com>
2016-06-02 16:20:34 +0200

    Initial commit

---
+++ core/src/de/gymwkb/civ/GameScreen.java
@@ -0,0 +1,6 @@
+package de.gymwkb.civ;
+
+import javafx.scene.Scene;
+
+public class GameScreen extends Scene {
+}
---
+++ desktop/src/de/gymwkb/civ/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.gymwkb.civ.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.gymwkb.civ.CivGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CivGame(), config);
+	}
+}
