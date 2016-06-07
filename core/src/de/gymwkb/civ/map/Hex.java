d9a1e9fb1084a594b3ab83e97a7fe884ad04ab5f
Dominik Stolz <dominik.stolz21@gmail.com>
2016-06-07 20:23:09 +0200

    hashCode und equals in Hex hinzugefügt

--- core/src/de/gymwkb/civ/map/Hex.java
+++ core/src/de/gymwkb/civ/map/Hex.java
@@ -15,4 +15,33 @@ public class Hex {
         return new Hex(q + other.q, r + other.r, s + other.s);
     }
 
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = 31 * result + q;
+        result = 31 * result + r;
+        result = 31 * result + s;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Hex other = (Hex) obj;
+        if (q != other.q)
+            return false;
+        if (r != other.r)
+            return false;
+        if (s != other.s)
+            return false;
+        return true;
+    }
+    
+    
+
 }
