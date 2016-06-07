8b7e9337a5c9a19d051d1f0494b9bc8eba9606e8
Dominik Stolz <dominik.stolz21@gmail.com>
2016-06-07 16:06:05 +0200

    Layout überarbeitet

--- core/src/de/gymwkb/civ/hexutils/Layout.java
+++ core/src/de/gymwkb/civ/hexutils/Layout.java
@@ -1,20 +1,20 @@
 package de.gymwkb.civ.hexutils;
 
-import java.util.ArrayList;
-
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
 import de.gymwkb.civ.Hex;
 
 public class Layout {
-    public static Orientation pointy = new Orientation((float) Math.sqrt(3.0), (float) Math.sqrt(3.0) / 2.0f, 0.0f,
-            3.0f / 2.0f, (float) Math.sqrt(3.0) / 3.0f, -1.0f / 3.0f, 0.0f, 2.0f / 3.0f, 0.5f);
-    public static Orientation flat = new Orientation(3.0f / 2.0f, 0.0f, (float) Math.sqrt(3.0) / 2.0f,
-            (float) Math.sqrt(3.0), 2.0f / 3.0f, 0.0f, -1.0f / 3.0f, (float) Math.sqrt(3.0) / 3.0f, 0.0f);
-    public final Orientation orientation;
-    public final Vector2 size;
-    public final Vector2 origin;
+    private static final float SQRT3 = (float) Math.sqrt(3.0);
+    public static final Orientation POINTY = new Orientation(SQRT3, SQRT3 / 2.0f, 0.0f, 3.0f / 2.0f, SQRT3 / 3.0f,
+            -1.0f / 3.0f, 0.0f, 2.0f / 3.0f, 0.5f);
+    public static final Orientation FLAT = new Orientation(3.0f / 2.0f, 0.0f, SQRT3 / 2.0f, SQRT3, 2.0f / 3.0f, 0.0f,
+            -1.0f / 3.0f, SQRT3 / 3.0f, 0.0f);
+
+    private Orientation orientation;
+    private Vector2 size;
+    private Vector2 origin;
 
     public Layout(Orientation orientation, Vector2 size, Vector2 origin) {
         this.orientation = orientation;
@@ -22,49 +22,52 @@ public class Layout {
         this.origin = origin;
     }
 
-    public static Vector2 hexToPixel(Layout layout, Hex h) {
-        Orientation M = layout.orientation;
-        Vector2 size = layout.size;
-        Vector2 origin = layout.origin;
-        float x = (M.f0 * h.q + M.f1 * h.r) * size.x;
-        float y = (M.f2 * h.q + M.f3 * h.r) * size.y;
+    public Vector2 hexToPixel(Hex h) {
+        float x = (orientation.f0 * h.q + orientation.f1 * h.r) * size.x;
+        float y = (orientation.f2 * h.q + orientation.f3 * h.r) * size.y;
         return new Vector2(x + origin.x, y + origin.y);
     }
 
-
-    public static FractionalHex pixelToHex(Layout layout, Vector2 p) {
-        Orientation M = layout.orientation;
-        Vector2 size = layout.size;
-        Vector2 origin = layout.origin;
+    public Hex pixelToHex(Vector2 p) {
         Vector2 pt = new Vector2((p.x - origin.x) / size.x, (p.y - origin.y) / size.y);
-        double q = M.b0 * pt.x + M.b1 * pt.y;
-        double r = M.b2 * pt.x + M.b3 * pt.y;
-        return new FractionalHex(q, r, -q - r);
+        float q = orientation.b0 * pt.x + orientation.b1 * pt.y;
+        float r = orientation.b2 * pt.x + orientation.b3 * pt.y;
+        return roundHex(q, r);
     }
 
+    private Hex roundHex(float qf, float rf) {
+        int q = MathUtils.round(qf);
+        int r = MathUtils.round(rf);
+        int s = MathUtils.round(-qf - rf);
+        double q_diff = Math.abs(q - qf);
+        double r_diff = Math.abs(r - rf);
+        double s_diff = Math.abs(s - (-qf - rf));
+        if (q_diff > r_diff && q_diff > s_diff) {
+            q = -r - s;
+        } else if (r_diff > s_diff) {
+            r = -q - s;
+        } else {
+            s = -q - r;
+        }
+        return new Hex(q, r, s);
+    }
 
-    public static Vector2 hexCornerOffset(Layout layout, int corner) {
-        Orientation M = layout.orientation;
-        Vector2 size = layout.size;
-        float angle = 2.0f * MathUtils.PI * (corner + M.start_angle) / 6;
+    private Vector2 vertexOffset(int corner) {
+        float angle = 2.0f * MathUtils.PI * (corner + orientation.start_angle) / 6;
         return new Vector2(size.x * MathUtils.cos(angle), size.y * MathUtils.sin(angle));
     }
 
-
-    public static ArrayList<Vector2> polygonCorners(Layout layout, Hex h) {
-        ArrayList<Vector2> corners = new ArrayList<Vector2>() {
-            {
-            }
-        };
-        Vector2 center = Layout.hexToPixel(layout, h);
+    public Vector2[] getVertices(Hex h) {
+        Vector2[] vertices = new Vector2[6];
+        Vector2 center = hexToPixel(h);
         for (int i = 0; i < 6; i++) {
-            Vector2 offset = Layout.hexCornerOffset(layout, i);
-            corners.add(new Vector2(center.x + offset.x, center.y + offset.y));
+            Vector2 offset = vertexOffset(i);
+            vertices[i] = new Vector2(center.x + offset.x, center.y + offset.y);
         }
-        return corners;
+        return vertices;
     }
 
-    public static class Orientation {
+    private static class Orientation {
         public final float f0;
         public final float f1;
         public final float f2;
