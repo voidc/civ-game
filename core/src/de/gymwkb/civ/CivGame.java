acd27dc5169fec14506ef97af551794baa69fb14
Dominik Stolz <dominik.stolz21@gmail.com>
2016-06-13 18:31:20 +0200

    AssetManager hinzugefügt

--- core/src/de/gymwkb/civ/CivGame.java
+++ core/src/de/gymwkb/civ/CivGame.java
@@ -1,14 +1,23 @@
 package de.gymwkb.civ;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 public class CivGame extends Game {
-    SpriteBatch batch;
+    public SpriteBatch batch;
+    public AssetManager assets;
+    
+    public static CivGame instance;
 
     @Override
     public void create() {
+        instance = this;
         batch = new SpriteBatch();
+        assets = new AssetManager();
+        loadAssets();
         setScreen(new GameScreen(this));
     }
 
@@ -16,9 +25,16 @@ public class CivGame extends Game {
     public void render() {
         super.render();
     }
+    
+    private void loadAssets() {
+        assets.load("hextures/pack.atlas", TextureAtlas.class);
+        assets.load("skin/uiskin.json", Skin.class);
+        assets.finishLoading();
+    }
 
     @Override
     public void dispose() {
         batch.dispose();
+        assets.dispose();
     }
 }
