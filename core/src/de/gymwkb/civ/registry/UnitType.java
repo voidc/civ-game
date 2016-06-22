fc7c1519bc636293d265a599d29b4cf3bb49df21
0liCom <0liCom@users.noreply.github.com>
2016-06-22 22:10:31 +0200

    UnitTypes hinzugefügt

--- core/src/de/gymwkb/civ/registry/UnitType.java
+++ core/src/de/gymwkb/civ/registry/UnitType.java
@@ -1,21 +1,29 @@
 package de.gymwkb.civ.registry;
 
 public enum UnitType {
-    TEST(Hexture.UNIT_BASE, 1, 100, 10, 10, 1);
-    
-    private UnitType(Hexture hexture, int movementRange, int maxHealth, int strength, int defence, int attackRange) {
+    TEST(Hexture.UNIT_BASE, 1, 100, 10, 10, 1, 1),
+    ARCHER(Hexture.UNIT_BASE, 3, 100, 10, 10, 3, 3),
+    PIKEMAN(Hexture.UNIT_BASE, 1, 100, 10, 10, 1, 1),
+    LIGHT_CAVALRY(Hexture.UNIT_BASE, 3, 100, 10, 10, 1, 2),
+    HEAVY_CAVALRY(Hexture.UNIT_BASE, 1, 100, 10, 10, 1, 2),
+    SWORDSMAN(Hexture.UNIT_BASE, 1, 100, 10, 10, 1, 1);
+
+    private UnitType(Hexture hexture, int movementRange, int maxHealth, int strength, int defence, int attackRange,
+            int viewRange) {
         this.hexture = hexture;
         this.movementRange = movementRange;
         this.maxHealth = maxHealth;
         this.strength = strength;
         this.defence = defence;
         this.attackRange = attackRange;
+        this.viewRange = viewRange;
     }
-    
+
     public final Hexture hexture;
     public final int movementRange;
     public final int maxHealth;
     public final int strength;
     public final int defence;
     public final int attackRange;
+    public final int viewRange;
 }
